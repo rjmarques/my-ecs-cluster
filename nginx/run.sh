15ee2e4c9cc0d26eb0bc780c9b344eb1cef6d71e
Ricardo Marques <ricardomarques377@gmail.com>
2021-03-23 19:37:08 +0000

    start cron service

--- nginx/run.sh
+++ nginx/run.sh
@@ -10,6 +10,8 @@ echo "$ME: running certbot after nginx startsup, to create new certificates for 
 # so we run certbot with a little delay to ensure it runs after nginx started
 (sleep 5 ; certbot --nginx -n --agree-tos --email contact@ricardomarques.dev --no-eff-email -d ricardomarques.dev -d www.ricardomarques.dev -d something.ricardomarques.dev --redirect) &
 
+service cron start
+
 echo "$ME: starting nginx"
 
 nginx -g "daemon off;"
