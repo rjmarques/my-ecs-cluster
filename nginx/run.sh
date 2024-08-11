84c82d3fdcede1825942212a717e6e040e52907e
rjmarques <ricardomarques377@gmail.com>
2024-08-11 23:00:00 +0100

    fixing ssl cert

--- nginx/run.sh
+++ nginx/run.sh
@@ -8,7 +8,7 @@ echo "$ME: running certbot after nginx startsup, to create new certificates for 
 
 # the container should be hooked to the nginx process
 # so we run certbot with a little delay to ensure it runs after nginx started
-(sleep 5 ; certbot --nginx -n --agree-tos --email contact@ricardomarques.dev --no-eff-email --expand -d ricardomarques.dev,www.ricardomarques.dev,something.ricardomarques.dev --redirect) &
+(sleep 5 ; certbot --nginx -n --agree-tos --email contact@ricardomarques.dev --no-eff-email --expand -d ricardomarques.dev,www.ricardomarques.dev,something.ricardomarques.dev,solar.ricardomarques.dev --redirect) &
 
 service cron start
 
