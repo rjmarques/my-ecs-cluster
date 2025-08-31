0f7191a6f520d7066be4460f90ce54701eedebb5
rjmarques <ricardomarques377@gmail.com>
2025-08-31 19:02:20 +0100

    remove personal website as a container

--- output.tf
+++ output.tf
@@ -7,10 +7,6 @@ output "nginx_ecr_repository_url" {
   value = module.nginx.ecr_repository_url
 }
 
-output "personal_website_ecr_repository_url" {
-  value = module.personal-website.ecr_repository_url
-}
-
 output "sotd_ecr_repository_url" {
   value = module.sotd.ecr_repository_url
 }
